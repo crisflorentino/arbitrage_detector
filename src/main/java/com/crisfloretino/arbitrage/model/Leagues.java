ae3c3773d70a6f751d7e3938382cd9fc80062263
crisflorentino <crisflorentino12@gmail.com>
2026-02-04 18:21:13 -0500

    leagues enum

---
+++ src/main/java/com/crisfloretino/arbitrage/model/Leagues.java
@@ -0,0 +1,28 @@
+package com.crisfloretino.arbitrage.model;
+
+import lombok.Getter;
+
+public enum Leagues {
+    NBA("NBA", 1),
+    NFL("NFL", 2),
+    NHL("NHL", 3),
+    MLB("MLB", 4);
+
+    @Getter
+    private final String name;
+    @Getter
+    private final int id;
+
+    Leagues(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public static Leagues fromId(int id) {
+        for (Leagues league : values()) {
+            if (league.id == id) { return league; }
+        }
+
+        throw new IllegalArgumentException("ID does not exist in League enums.");
+    }
+}
